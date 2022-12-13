3242c21a45d2bedb0e7e084639ed37bb6021bd43
Johan Lundgren <lundgren.johan99@gmail.com>
2022-12-13 21:55:38 +0100

    add(day13): Solution to day 13

--- day13/solution.py
+++ day13/solution.py
@@ -1,43 +1,61 @@
-import json
+from functools import cmp_to_key
 
 with open("input.txt", "r") as f:
-    lines = [line.strip() for line in f.readlines()]
+    packetGroups = f.read().strip()
 
 packets = []
 
-for i in range(len(lines)):
-    if lines[i] == "":
-        packets.append([
-            json.loads(lines[i - 2]),
-            json.loads(lines[i - 1])
-        ])
-
-
 def checkPair(left, right):
-    valid = True
+    if isinstance(left, int) and isinstance(right, int):
+        if left < right:
+            return -1
+        elif left == right:
+            return 0
+        else:
+            return 1
+    elif isinstance(left, list) and isinstance(right, list):
+        i = 0
+        while i < len(left) and i < len(right):
+            c = checkPair(left[i], right[i])
+            if c == -1:
+                return -1
+            if c == 1:
+                return 1
+            i += 1
+        if i == len(left) and i < len(right):
+            return -1
+        elif i == len(right) and i < len(left):
+            return 1
+        else:
+            return 0
+    elif isinstance(left, int) and isinstance(right, list):
+        return checkPair([left], right)
+    else:
+        return checkPair(left, [right])
 
-    for i in range(len(left)):
-        if isinstance(left[i], int) and isinstance(right[i], int):
-            if left[i] == right[i] or left[i] < right[i]:
-                continue
-        
-        if isinstance(left[i], list) and isinstance(right[i], int) or isinstance(right[i], list) and isinstance(left[i], int):
-            if isinstance(left[i], list) and not checkPair(left[i], [right[i]]):
-                valid = False
-                break
-            elif isinstance(right[i], list) and not checkPair(right[i], [left[i]]):
-                valid = False
-                break
 
-    return valid
+part1 = 0
+for i, g in enumerate(packetGroups.split("\n\n")):
+    left, right = g.split("\n")
 
+    left = eval(left)
+    right = eval(right)
 
-for p in packets:
-    left = p[0]
-    right = p[1]
+    packets.append(left)
+    packets.append(right)
 
-    print(type(left))
-    print(isinstance(left, list))
+    if checkPair(left, right) == -1:
+        part1 += 1 + i
 
-    valid = checkPair(left, right)
-    print("valid: " + str(valid))
+print("Part 1: " + str(part1))
+
+packets.append([[2]])
+packets.append([[6]])
+
+packets = sorted(packets, key=cmp_to_key(lambda left, right: checkPair(left, right)))
+part2 = 1
+for i, p in enumerate(packets):
+    if p == [[2]] or p == [[6]]:
+        part2 *= i + 1
+
+print("Part 2: " + str(part2))
